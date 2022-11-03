a53e162382d671ea4caf9943bb76d8aaf56df940
iStagnant <ghafarielias200676@gmail.com>
2022-11-03 18:56:40 +0200

    removed quit function

--- .config/dwm/dwm.c
+++ .config/dwm/dwm.c
@@ -204,7 +204,6 @@ static void movemouse(const Arg *arg);
 static Client *nexttiled(Client *c);
 static void pop(Client *);
 static void propertynotify(XEvent *e);
-static void quit(const Arg *arg);
 static Monitor *recttomon(int x, int y, int w, int h);
 static void resize(Client *c, int x, int y, int w, int h, int interact);
 static void resizeclient(Client *c, int x, int y, int w, int h);

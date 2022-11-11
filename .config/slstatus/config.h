c5bf8da518e19f6994daa7a36e8a413b10d40f2c
iStagnant <ghafarielias200676@gmail.com>
2022-11-11 22:37:53 +0200

    replaced alsa-utils with pipewire

--- .config/slstatus/config.h
+++ .config/slstatus/config.h
@@ -66,8 +66,9 @@ static const struct arg args[] = {
 	//{ datetime, "%s",           "%F %T" },
 	//{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
     //{ netspeed_rx, "%sB/s  ", "enp0s3" },
-	{ run_command, ": %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+	{ run_command, ": %4s | ", "pamixer --get-volume-human" },
 	{ cpu_perc, "[CPU  %s%%]   ", NULL	      },
 	{ ram_perc, "[RAM  %s%%]   ", NULL	      },
 	{ datetime, "%s",           "%a %b %d %r" },
 };
+

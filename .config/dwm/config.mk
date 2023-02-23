a7e1860496cbcb6e3499d88c582290a6850e8766
iStagnant <ghafarielias200676@gmail.com>
2023-02-23 15:41:03 +0200

    Bumped dwm to version 6.4 last commit patched '348f6559ab0d4793db196ffa56ba96ab95a594a6'

--- .config/dwm/config.mk
+++ .config/dwm/config.mk
@@ -1,5 +1,5 @@
 # dwm version
-VERSION = 6.2
+VERSION = 6.4
 
 # Customize below to fit your system
 
@@ -19,6 +19,7 @@ FREETYPELIBS = -lfontconfig -lXft
 FREETYPEINC = /usr/include/freetype2
 # OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
+#MANPREFIX = ${PREFIX}/man
 #KVMLIB = -lkvm
 
 # includes and libs
@@ -26,7 +27,7 @@ INCS = -I${X11INC} -I${FREETYPEINC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lX11-xcb -lxcb -lxcb-res ${KVMLIB}
 
 # flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
